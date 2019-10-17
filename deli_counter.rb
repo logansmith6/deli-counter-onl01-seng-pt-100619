46182f3fe1c46917d8efca0b1dd5c25e6850c2df
Logan Smith <logsmith06@gmail.com>
2019-10-17 20:44:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ if katz_deli.size == 0
   puts "The line is currently empty."
 elsif katz_deli.size > 0
   puts "The line is currently: "
-  katz_deli.size.times do 
+  katz_deli.size.times do
       puts "#{counter + 1}. #{katz_deli[counter]}"
 end
 
