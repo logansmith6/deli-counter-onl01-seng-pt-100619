744ee6177e74e4642f2808a4449ea094c8dc36bd
Logan Smith <logsmith06@gmail.com>
2019-10-17 20:41:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,6 @@ counter = 1
 if katz_deli.size == 0
   puts "The line is currently empty."
 elsif katz_deli.size > 0
-  puts "The line is currently: "
   katz_deli.each do |name|
     puts "#{counter}. #{name}"
   end
