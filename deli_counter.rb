52e5b83416f38f554a16149ff772cdb85812c0fe
Logan Smith <logsmith06@gmail.com>
2019-10-17 20:40:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,13 +12,13 @@ end
 counter = 1
 if katz_deli.size == 0
   puts "The line is currently empty."
-elsif katz_deli.size > 0 {
+elsif katz_deli.size > 0
   puts "The line is currently: "
   katz_deli.each do |name|
     puts "#{counter}. #{name}"
   end
 end
-}
+
 end
 
 def take_a_number(katz_deli, new_customer)
